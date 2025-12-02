b43033b62ce35292856b1677a858eb7d401f10fe
van-sprundel <ramonvansprundel@gmail.com>
2025-12-02 18:56:49 +0100

    unify

--- src/days/day2/mod.rs
+++ src/days/day2/mod.rs
@@ -1,32 +1,12 @@
 pub fn part1(input: &str) {
-    let mut invalid_id_acc = 0;
-    input.split(',').for_each(|range| {
-        let divider_index = range.find('-');
-        if let Some(index) = divider_index {
-            let left = range[..index].trim().parse::<usize>().unwrap();
-            let right = range[index + 1..].trim().parse::<usize>().unwrap();
-
-            let mut index = left;
-            loop {
-                if index > right {
-                    break;
-                }
-
-                if !is_valid_id(&index.to_string()) {
-                    invalid_id_acc += index;
-                }
-
-                index += 1;
-            }
-        }
-    });
-
-    println!("{invalid_id_acc}")
+    solution(input, Part::PART1)
 }
-
 pub fn part2(input: &str) {
-    let mut invalid_id_acc = 0;
+    solution(input, Part::PART2)
+}
 
+fn solution(input: &str, part: Part) {
+    let mut invalid_id_acc = 0;
     input.split(',').for_each(|range| {
         let divider_index = range.find('-');
         if let Some(index) = divider_index {
@@ -39,7 +19,7 @@ pub fn part2(input: &str) {
                     break;
                 }
 
-                if !is_valid_id_part2(&index.to_string()) {
+                if !is_valid_id(&index.to_string(), part.clone()) {
                     invalid_id_acc += index;
                 }
 
@@ -51,32 +31,31 @@ pub fn part2(input: &str) {
     println!("{invalid_id_acc}")
 }
 
-fn is_valid_id(n: &str) -> bool {
-    let chars = n.chars().collect::<Vec<_>>();
-    let len = chars.len();
-
-    // it can't be the same code twice if the char lengths are uneven
-    // since if we grab left and right they will never match
-    if len % 2 == 1 {
-        return true;
-    }
-
-    // if ..(len/2) and (len/2).. are the same, continue
-    let len_h = (len / 2);
-    let left = &chars[..len_h];
-    let right = &chars[len_h..];
-
-    *left != *right
+#[derive(PartialEq, Clone)]
+enum Part {
+    PART1,
+    PART2,
 }
 
-/// s == s[0:d] repeated n/d times
-fn is_valid_id_part2(n: &str) -> bool {
+fn is_valid_id(n: &str, part: Part) -> bool {
     let bytes = n.as_bytes();
     let len = bytes.len();
 
     // we now need to find all denominators i guess because i can match three four and five times
     // so len/2 can match, but also len/3 len/4 .. len/n (1)
-    for d in 1..len {
+    let (start_d, end_d) = match part {
+        /// s == s[0:2] repeated n/2 times
+        Part::PART1 => {
+            if len % 2 == 1 {
+                return true;
+            }
+            let d = len / 2;
+            (d, d + 1)
+        }
+        /// s == s[0:d] repeated n/d times
+        Part::PART2 => (1, len),
+    };
+    for d in start_d..end_d {
         // this can NEVER match since we can't get d chunks of the string
         // e.g. 1231234 we cannot get a left,right slice because it's uneven
         // this is fine to skip
