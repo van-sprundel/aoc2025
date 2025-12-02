df82a0c47588aa395ac09fd5e70f24ba89a41328
van-sprundel <ramonvansprundel@gmail.com>
2025-12-02 20:29:54 +0100

    we're branchless!

--- src/days/day2/mod.rs
+++ src/days/day2/mod.rs
@@ -13,7 +13,7 @@ fn solution(input: &str, part: Part) {
             let right = range[index + 1..].trim().parse::<usize>().unwrap();
 
             invalid_id_acc += (left..right)
-                .map(|i| (!is_valid_id(&i.to_string(), part.clone()) as usize) * i)
+                .map(|i| (!is_valid_id(&i.to_string(), part) as usize) * i)
                 .sum::<usize>()
         });
     });
@@ -22,7 +22,7 @@ fn solution(input: &str, part: Part) {
 }
 
 // enums arent real xd
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Copy, Clone)]
 enum Part {
     PART1 = 0,
     PART2 = 1,
@@ -40,21 +40,40 @@ fn is_valid_id(n: &str, part: Part) -> bool {
     // for part2 we need to iter 1..d+1
     // both ends can be d+1 because we can't surpass d (we can't make less than 2 chunks)
     let d = len / 2;
-    let start_d = (part.clone() as u8 == 0) as usize * d + ((part.clone() as u8 & 1) as usize);
+    let is_part1 = (part as u8) ^ 1;
+    let is_part2 = part as u8;
+
+    // part1: start_d = d,
+    // part2: start_d = 1
+    let start_d = is_part1 as usize * d + is_part2 as usize;
     let end_d = d + 1;
 
     // part 1 has a specific condition where if the length doesnt match we continue
     // this also applies for part 2 but for chunk d
     // for some reason len.is_multiple_of doesn't cover this
-    (part == Part::PART1 && len % 2 == 1)
-        || (start_d..end_d).all(|d| {
-            // this can NEVER match since we can't get d chunks of the string
-            // e.g. 1231234 we cannot get a left,right slice because it's uneven
-            // this is fine to skip
-            !len.is_multiple_of(d)
+    let part1_invalid = is_part1 as usize & (len & 1);
+
+    let mut all_divisors_invalid = 1usize;
+    for divisor in start_d..end_d {
+        // this can NEVER match since we can't get d chunks of the string
+        // e.g. 1231234 we cannot get a left,right slice because it's uneven
+        // this is fine to skip
+        let not_multiple = (!len.is_multiple_of(divisor)) as usize;
+
         // then we can do the actual logic where we check if any chunk does NOT match the expected chunk
-            || (d..=len - d)
-                .step_by(d)
-                .any(|i| bytes[i..i + d] != bytes[..d])
-        })
+        let mut any_mismatch = 0usize;
+        let mut i = divisor;
+        while divisor > 0 && i + divisor <= len {
+            let mut chunk_mismatch = 0usize;
+            for j in 0..divisor {
+                chunk_mismatch |= (bytes[i + j] ^ bytes[j]) as usize;
+            }
+            any_mismatch |= (chunk_mismatch != 0) as usize;
+            i += divisor;
+        }
+
+        all_divisors_invalid &= not_multiple | any_mismatch;
+    }
+
+    (part1_invalid | all_divisors_invalid) != 0
 }
