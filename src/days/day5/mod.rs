4c070078c7906e0308670a0ad03d8177e58fa136
van-sprundel <ramonvansprundel@gmail.com>
2025-12-06 10:03:19 +0100

    workie

--- src/days/day5/mod.rs
+++ src/days/day5/mod.rs
@@ -1,7 +1,59 @@
+use std::collections::HashSet;
+
 pub fn part1(input: &str) {
-    todo!("Day 5 part 1 not implemented yet");
+    println!("{}", solution(input, true));
 }
 
 pub fn part2(input: &str) {
-    todo!("Day 5 part 2 not implemented yet");
+    println!("{}", solution(input, false));
+}
+
+fn solution(input: &str, include_ids: bool) -> usize {
+    let [ranges, ids] = input.split("\n\n").collect::<Vec<_>>()[..] else {
+        panic!("Invalid input")
+    };
+
+    let mut ranges: Vec<(usize, usize)> = ranges
+        .lines()
+        .map(|range| {
+            let [left, right] = range
+                .split('-')
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()[..]
+            else {
+                panic!("Invalid range")
+            };
+            (left, right)
+        })
+        .collect();
+
+    if !include_ids {
+        // count total IDs in all ranges (need to merge overlapping ranges)
+        ranges.sort_by_key(|(left, _)| *left);
+
+        let mut merged: Vec<(usize, usize)> = Vec::new();
+        for (left, right) in ranges {
+            if let Some(last) = merged.last_mut() {
+                if left <= last.1 + 1 {
+                    last.1 = last.1.max(right);
+                } else {
+                    merged.push((left, right));
+                }
+            } else {
+                merged.push((left, right));
+            }
+        }
+
+        return merged.iter().map(|(left, right)| right - left + 1).sum();
+    }
+
+    // Part 1: Count IDs from list that fall in ranges
+    let mut count = 0;
+    for id in ids.lines().map(|id| id.parse::<usize>().unwrap()) {
+        count += (ranges
+            .iter()
+            .any(|(left, right)| id >= *left && id <= *right)) as usize;
+    }
+
+    count
 }
